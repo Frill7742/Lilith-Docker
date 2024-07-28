6a85beee02b5444f26fa8152d411c9b3e5f0ed56
MSGP <166161297+Frill7742@users.noreply.github.com>
2024-07-28 16:54:31 +0200

    added the files from my computer

---
+++ Lilith/start-lilith.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cd /home/alex/.container/lilith/
+
+sudo docker compose up -d
+
+if [ -z "$( ls -A '/home/alex/.container/lilith/ssl/' )" ]; then
+   sudo docker exec -it lilith-lilith-1 apt-get update
+   sudo docker exec -it lilith-lilith-1 apt-get install -y --no-install-recommends ca-certificates
+   sudo docker exec -it lilith-lilith-1 update-ca-cerificates
+else
+   echo "Not Empty"
+fi
+
+sudo docker exec -t lilith-lilith-1 /lilith/lilith-launcher-linux-s3
---
+++ Lilith/stop-lilith.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo docker stop lilith-lilith-1
+
+sudo docker rm lilith-lilith-1
+
+echo "Lilith and the container stopped"
