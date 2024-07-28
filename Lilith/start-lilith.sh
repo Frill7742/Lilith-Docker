e23bc33d0ff75502669b3485ddd5c5ab40556784
MSGP <166161297+Frill7742@users.noreply.github.com>
2024-07-28 17:35:52 +0000

    Added a installing message for the user

--- Lilith/start-lilith.sh
+++ Lilith/start-lilith.sh
@@ -6,6 +6,9 @@ sudo docker compose up -d
 
 
 if [ -z "$( ls -A ~/.container/lilith/ssl/ )" ]; then
+   echo "installing ... "
+   echo "Please wait a few moments."
+
    #At the moment, Ubuntu does not have ca-certificates installed, so it will be installed at the first timeAt the moment, Ubuntu does not have ca-certificates installed, so it will be installed at the first time.
    sudo docker exec -it lilith-lilith-1 apt-get update >> /dev/null
    sudo docker exec -it lilith-lilith-1 apt-get install -y --no-install-recommends ca-certificates >> /dev/null
