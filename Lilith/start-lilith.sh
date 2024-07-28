a6078f5a7affbc39e46f337480b08cfd714be5af
MSGP <166161297+Frill7742@users.noreply.github.com>
2024-07-28 15:08:14 +0000

    replaced my name with an span for it

--- Lilith/start-lilith.sh
+++ Lilith/start-lilith.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-cd /home/alex/.container/lilith/
+cd /home/{user}/.container/lilith/
 
 sudo docker compose up -d
 
-if [ -z "$( ls -A '/home/alex/.container/lilith/ssl/' )" ]; then
+if [ -z "$( ls -A '/home/{user}/.container/lilith/ssl/' )" ]; then
    sudo docker exec -it lilith-lilith-1 apt-get update
    sudo docker exec -it lilith-lilith-1 apt-get install -y --no-install-recommends ca-certificates
    sudo docker exec -it lilith-lilith-1 update-ca-cerificates
