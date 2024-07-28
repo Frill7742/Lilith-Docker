c43ea2181e460ed1d33e6ddff57a80db762d1e86
MSGP <166161297+Frill7742@users.noreply.github.com>
2024-07-28 17:26:50 +0000

    fixed a typo

--- Lilith/start-lilith.sh
+++ Lilith/start-lilith.sh
@@ -9,7 +9,7 @@ if [ -z "$( ls -A ~/.container/lilith/ssl/ )" ]; then
    #At the moment, Ubuntu does not have ca-certificates installed, so it will be installed at the first timeAt the moment, Ubuntu does not have ca-certificates installed, so it will be installed at the first time.
    sudo docker exec -it lilith-lilith-1 apt-get update
    sudo docker exec -it lilith-lilith-1 apt-get install -y --no-install-recommends ca-certificates
-   sudo docker exec -it lilith-lilith-1 update-ca-cerificates
+   sudo docker exec -it lilith-lilith-1 update-ca-certificates
 else
    echo "ca certificates are installed."
 fi
